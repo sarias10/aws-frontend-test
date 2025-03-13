43cdd6b1783c6859f11ea152bbfe70a6300829d2
Sergio Arias <s-andres140@hotmail.com>
2025-03-13 18:41:17 -0500

    refactor: :sparkles: enhance SideMenu with create dropdown functionality; update styles for better layout and interaction

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -2,11 +2,14 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import { AuthContext } from '../../context/authContext';
 import { Link } from 'react-router-dom';
 import styles from './SideMenu.module.css';
+
 export const SideMenu = () => {
     // Obtener el contexto de autenticación
     const authContex = useContext(AuthContext);
     // Estado para controlar si el desplegable de búsqueda está abierto
     const [ isSearchOpen, setIsSearchOpen ] = useState(false);
+
+    const [ isCreateOpen, setIsCreateOpen ] = useState(false);
     // Referencias para el botón de búsqueda y el dropdown
     const searchRef = useRef<HTMLLIElement>(null);
     const dropdownRef = useRef<HTMLDivElement>(null);
@@ -16,7 +19,7 @@ export const SideMenu = () => {
         throw new Error('Error al cargar SideMenu');
     }
 
-    const { token, logout } = authContex;
+    const { logout } = authContex;
 
     // Función para manejar el cierre de sesión
     const handleLogout = () => {
@@ -27,6 +30,11 @@ export const SideMenu = () => {
     const toggleSearch = (event: React.MouseEvent) => {
         event.preventDefault();
         setIsSearchOpen(!isSearchOpen);
+    };
+
+    const toggleCreate = (event: React.MouseEvent) => {
+        event.preventDefault();
+        setIsCreateOpen(!isCreateOpen);
     };
 
     // Cerrar el dropdown si se hace clic fuera de él
@@ -53,29 +61,31 @@ export const SideMenu = () => {
     return (
         <div className={styles['side-menu']}>
             <ul>
-                {token? (
-                    <>
-                        <li></li>
+                <>
+                    <div className={styles['div-logo']}>
+                        <li><Link to='/'>InstagramDemake</Link></li>
+                    </div>
+                    <div className={styles['div-other-options']}>
                         <li><Link to='/'>Home</Link></li>
-                        <li ref={searchRef} onClick={toggleSearch}>Search</li>
+                        <li ref={searchRef} onClick={toggleSearch} className={styles['searchStyles']}>Search</li>
                         {isSearchOpen && (
                             <div ref={dropdownRef}  className={styles['dropdown']}>
                                 <input type="text" placeholder='Search...' />
                             </div>
                         )
-
                         }
                         <li><Link to='user-profile'>Profile</Link></li>
-
+                        <li onClick={toggleCreate} className={styles['createStyles']}>Create</li>
+                        {isCreateOpen && (
+                            <div className={styles['createDropdown']}>
+                                <li>Post</li>
+                                <li>Story</li>
+                            </div>
+                        )
+                        }
                         <li onClick={handleLogout}><Link to='/'>Log out</Link></li>
-                    </>
-                ):(
-                    <>
-                        <li><Link to='/'>Public tweets</Link></li>
-                        <li><Link to='login'>Login</Link></li>
-                        <li><Link to='signup'>Sign Up</Link></li>
-                    </>
-                )}
+                    </div>
+                </>
             </ul>
         </div>
     );
