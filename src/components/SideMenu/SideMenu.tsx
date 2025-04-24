fa1f42f55d36e7760a96546e3c30731db3ee5eb9
Sergio Arias <s-andres140@hotmail.com>
2025-04-23 21:14:22 -0500

    fix: :bug: ensure smooth scrolling to profile on profile click

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -65,6 +65,7 @@ export const SideMenu = () => {
 
     const handleProfile = () => {
         setActive('profile');
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
         navigate(`${username}`);
     };
 
